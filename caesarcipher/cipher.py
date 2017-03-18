4b21b3a5dd020fb2621ef9a0b92963adddee100f
Nikolas <nxexenis@gmail.com>
2017-03-18 18:43:24 +1100

    Added input method

--- caesarcipher/cipher.py
+++ caesarcipher/cipher.py
@@ -1,1 +1,16 @@
-print("Unimplemented!")
+import sys
+
+def main():
+
+    try:
+        shift = int(input('Shift value: '))
+        text = str(input('Text: '))
+    except ValueError:
+        print("Invalid input.")
+        sys.exit(1)
+
+    # Debug
+    print("Shift: " + str(shift))
+    print("Text: " + text)
+
+if __name__ == "__main__" : main()
