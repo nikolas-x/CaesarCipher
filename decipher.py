43361c601547e1ee676fadb0d15165377fcc0b95
Nikolas <nxexenis@gmail.com>
2017-03-23 21:32:35 +1100

    Added text reception

--- decipher.py
+++ decipher.py
@@ -1,5 +1,40 @@
+import sys
+
+
 def main():
-    print("Unimplemented")
+    # Take in the text
+    try:
+        print("Text (type ENDTEXT at the end of the text): ")
+        text = []
+
+        # We take in an array of text lines
+        # We ignore newlines and break when the user types 'ENDTEXT'
+        # TODO: Refactor repeated code to a class
+        while True:
+            try:
+                line = sys.stdin.readline()
+            except KeyboardInterrupt:
+                break
+
+            if not line:
+                break
+
+            if line == '\n':
+                continue
+
+            line = line.rstrip()
+
+            if line.upper() == 'ENDTEXT':
+                break
+
+            text.append(line)
+    except ValueError:
+        print("Invalid input.")
+        sys.exit(1)
+
+    combinedText = '\n'.join(text)
+    print(combinedText)
+
 
 if __name__ == "__main__":
     main()
