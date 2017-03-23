bd320ccb093775de619e146a49b989404fcbb976
Nikolas <nxexenis@gmail.com>
2017-03-23 21:27:16 +1100

    Added ability to receive and output multiple lines

--- caesarcipher/cipher.py
+++ caesarcipher/cipher.py
@@ -1,17 +1,41 @@
 import sys
 
+
 def main():
     # Take in the shift value and the text
     try:
         shift = int(input('Shift value: '))
-        text = str(input('Text: '))
+        print("Text (type ENDTEXT at the end of the text): ")
+        text = []
+
+        # We take in an array of text lines
+        # We ignore newlines and break when the user types 'ENDTEXT'
+        while True:
+            try:
+                line = sys.stdin.readline()
+            except KeyboardInterrupt:
+                break
+
+            if not line:
+                break
+
+            if line == '\n':
+                continue
+
+            line = line.rstrip()
+
+            if line.upper() == 'ENDTEXT':
+                break
+
+            text.append(line)
     except ValueError:
         print("Invalid input.")
         sys.exit(1)
 
     table = build_translation_table(shift)
-    ciphered = text.upper().translate(table)
-    print(ciphered)
+    ciphered = [cipher(table, line) for line in text]
+    print(*ciphered, sep='\n')
+
 
 # Generates a table containing each letter of the alphabet shifted by the given shift value
 def build_translation_table(shift):
@@ -23,6 +47,10 @@ def build_translation_table(shift):
         key.append(alphabet[(i + shift) % 26])
 
     return str.maketrans(alphabet, ''.join(key))
+
+
+def cipher(table, line):
+    return line.upper().translate(table)
 
 
 if __name__ == "__main__":
